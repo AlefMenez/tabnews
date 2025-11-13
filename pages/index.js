6fa0d1edac4eaac4e72175bf78befaf21ae9948e
Alef Menezes <107272586+AlefMenez@users.noreply.github.com>
2025-11-13 12:09:55 +0000

    Modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function home () {
-    return <h1>Parabéns pelo seu novo cargo, voltado a prática jurídica</h1>
+    return <h1>Parabéns pelo seu novo cargo, voltado a prática jurídica! </h1>
 }
 
 export default home;
